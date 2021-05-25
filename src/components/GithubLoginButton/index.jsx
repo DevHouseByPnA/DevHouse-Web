e2b8e4d536edac5c2e97fc433cbaa78cf8815835
Aditya Pratap <adityapratap7344@gmail.com>
2021-05-25 18:35:01 +0530

    add firebase auth

--- src/components/GithubLoginButton/index.jsx
+++ src/components/GithubLoginButton/index.jsx
@@ -1,9 +1,10 @@
-import styles from './styles.module.css';
 import { AiFillGithub } from 'react-icons/ai';
+import styles from './styles.module.css';
+import { signInWithGithub } from '../../utils/firebase';
 
 export const GithubLoginButton = () => {
     return (
-        <button className={styles['button']}>
+        <button className={styles['button']} onClick={signInWithGithub}>
             <AiFillGithub className={styles['icon']} />
             <span>Login with Github</span>
         </button>
---
+++ src/utils/firebase.js
@@ -0,0 +1,69 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDWtLEWWIUwnDsrLg8-ZZDRcpsxyLjOkXs",
+    authDomain: "devhouse-demo.firebaseapp.com",
+    projectId: "devhouse-demo",
+    storageBucket: "devhouse-demo.appspot.com",
+    messagingSenderId: "488856086794",
+    appId: "1:488856086794:web:a268a80eaa2f5730c672f3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firebaseAuth = firebase.auth();
+const githubAuthProvider = new firebase.auth.GithubAuthProvider();
+githubAuthProvider.addScope('repo');
+
+export const signInWithGithub = async () => {
+    try {
+        await firebaseAuth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+        const result = await firebaseAuth.signInWithPopup(githubAuthProvider);
+
+        /** @type {firebase.auth.OAuthCredential} */
+        const credential = result.credential;
+
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const token = credential.accessToken;
+
+        const user = result.user;
+
+        console.log({
+            user,
+            token,
+        });
+
+        return { user, token };
+    } catch (error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        const credential = error.credential;
+
+        console.log({
+            errorCode,
+            errorMessage,
+            email,
+            credential,
+        });
+
+        return {
+            errorCode,
+            errorMessage,
+            email,
+            credential,
+        };
+    }
+}
+
+export const signOut = async () => {
+    try {
+        await firebase.auth().signOut();
+    } catch (error) {
+        console.log(error);
+    }
+}
